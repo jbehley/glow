8205b68a49d76cbd0d1b58fe67003041d9110d9a
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2018-03-12 19:48:45 +0100

    Add a way to load shader into cache from file

--- src/glow/GlShaderCache.cpp
+++ src/glow/GlShaderCache.cpp
@@ -1,6 +1,7 @@
 #include "GlShaderCache.h"
 
 #include <iostream>
+#include <fstream>
 
 namespace glow {
 
@@ -20,6 +21,24 @@ std::string GlShaderCache::getSource(const std::string& filename) {
   std::string b = basename(filename);
 
   return sources_[b];
+}
+
+void GlShaderCache::insertSource(const std::string& filename) {
+  std::ios::openmode mode = std::ios::in | std::ios::ate;
+  std::ifstream ifs(filename, mode);
+  std::string source;
+  if (ifs) {
+    const std::streamsize size = static_cast<const std::streamsize>(ifs.tellg());
+    ifs.seekg(0, std::ios::beg);
+    source.resize(size);
+    ifs.read(const_cast<char*>(source.data()), size);
+    source.resize(ifs.gcount());
+    ifs.close();
+  } else {
+    std::cerr << "Error: Reading from file '" << filename << "' failed." << std::endl;
+    source = "";
+  }
+  insertSource(filename, source);
 }
 
 void GlShaderCache::insertSource(const std::string& filename, const std::string& source) {
