c25cda1e6d012c21487cf3d7e80461e2dc58f1f7
Meher Malladi <meher.malladi@uni-bonn.de>
2023-02-02 15:02:31 +0100

    fix (restore) header include order to original

--- test/framebuffer-test.cpp
+++ test/framebuffer-test.cpp
@@ -10,50 +10,50 @@ using namespace glow;
 namespace {
 
 TEST(FrambufferTest, initTest) {
-  GlFramebuffer buf(10, 10);
-  GlRenderbuffer rbo(10, 10, RenderbufferFormat::DEPTH_STENCIL);
-  buf.attach(FramebufferAttachment::DEPTH_STENCIL, rbo);
-  // note: as always...not a framebuffer until first time bound.
-  buf.bind();
-  buf.release();
+    GlFramebuffer buf(10, 10);
+    GlRenderbuffer rbo(10, 10, RenderbufferFormat::DEPTH_STENCIL);
+    buf.attach(FramebufferAttachment::DEPTH_STENCIL, rbo);
+    // note: as always...not a framebuffer until first time bound.
+    buf.bind();
+    buf.release();
 
-  ASSERT_TRUE(glIsFramebuffer(buf.id()));
-  ASSERT_NO_THROW(CheckGlError());
+    ASSERT_TRUE(glIsFramebuffer(buf.id()));
+    ASSERT_NO_THROW(CheckGlError());
 }
 
 TEST(FramebufferTest, testStatelessness) {
-  GlState priorState = GlState::queryAll();
-  CheckGlError();
+    GlState priorState = GlState::queryAll();
+    CheckGlError();
 
-  GlFramebuffer buf(640, 480);
-  if (priorState != GlState::queryAll()) priorState.difference(GlState::queryAll());
-  ASSERT_EQ(true, (priorState == GlState::queryAll()));
+    GlFramebuffer buf(640, 480);
+    if (priorState != GlState::queryAll()) priorState.difference(GlState::queryAll());
+    ASSERT_EQ(true, (priorState == GlState::queryAll()));
 
-  GlTexture texture(640, 480, TextureFormat::RGBA);
-  GlRenderbuffer rbo(640, 480, RenderbufferFormat::DEPTH_STENCIL);
-  if (priorState != GlState::queryAll()) priorState.difference(GlState::queryAll());
-  ASSERT_EQ(true, (priorState == GlState::queryAll()));
+    GlTexture texture(640, 480, TextureFormat::RGBA);
+    GlRenderbuffer rbo(640, 480, RenderbufferFormat::DEPTH_STENCIL);
+    if (priorState != GlState::queryAll()) priorState.difference(GlState::queryAll());
+    ASSERT_EQ(true, (priorState == GlState::queryAll()));
 
-  buf.attach(FramebufferAttachment::COLOR0, texture);
-  //  glBindFramebuffer(GL_FRAMEBUFFER, 0);
-  //  glFinish();
-  if (priorState != GlState::queryAll()) priorState.difference(GlState::queryAll());
-  ASSERT_EQ(true, (priorState == GlState::queryAll()));
+    buf.attach(FramebufferAttachment::COLOR0, texture);
+    //  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    //  glFinish();
+    if (priorState != GlState::queryAll()) priorState.difference(GlState::queryAll());
+    ASSERT_EQ(true, (priorState == GlState::queryAll()));
 
-  buf.attach(FramebufferAttachment::DEPTH_STENCIL, rbo);
-  //  glBindFramebuffer(GL_FRAMEBUFFER, 0);
-  //  glFinish();
-  if (priorState != GlState::queryAll()) priorState.difference(GlState::queryAll());
-  ASSERT_EQ(true, (priorState == GlState::queryAll()));
+    buf.attach(FramebufferAttachment::DEPTH_STENCIL, rbo);
+    //  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    //  glFinish();
+    if (priorState != GlState::queryAll()) priorState.difference(GlState::queryAll());
+    ASSERT_EQ(true, (priorState == GlState::queryAll()));
 
-  buf.bind();
+    buf.bind();
 
-  buf.release();
-  if (priorState != GlState::queryAll()) priorState.difference(GlState::queryAll());
-  ASSERT_EQ(true, (priorState == GlState::queryAll()));
+    buf.release();
+    if (priorState != GlState::queryAll()) priorState.difference(GlState::queryAll());
+    ASSERT_EQ(true, (priorState == GlState::queryAll()));
 
-  buf.valid();
-  if (priorState != GlState::queryAll()) priorState.difference(GlState::queryAll());
-  ASSERT_EQ(true, (priorState == GlState::queryAll()));
+    buf.valid();
+    if (priorState != GlState::queryAll()) priorState.difference(GlState::queryAll());
+    ASSERT_EQ(true, (priorState == GlState::queryAll()));
 }
 }  // namespace
--- test/main.cpp
+++ test/main.cpp
@@ -1,25 +1,27 @@
-#include <glow/GlCapabilities.h>
-#include <glow/GlState.h>
-#include <glow/glbase.h>
-#include <glow/util/X11OffscreenContext.h>
 #include <gtest/gtest.h>
+
+#include <glow/glbase.h>
+
+#include <glow/util/X11OffscreenContext.h>
+#include <glow/GlState.h>
+#include <glow/GlCapabilities.h>
 
 using namespace glow;
 
 int main(int argc, char** argv) {
-  X11OffscreenContext ctx(3, 3);
+    X11OffscreenContext ctx(3, 3);
 
-  /* try it out */
-  inititializeGLEW();
+    /* try it out */
+    inititializeGLEW();
 
-  if (argc > 1 && std::string(argv[1]) == "info") {
-    std::cout << "Device's OpenGL Capabilities: \n" << std::endl;
-    std::cout << GlCapabilities::getInstance() << std::endl;
+    if (argc > 1 && std::string(argv[1]) == "info") {
+        std::cout << "Device's OpenGL Capabilities: \n" << std::endl;
+        std::cout << GlCapabilities::getInstance() << std::endl;
 
-    std::cout << "Device's current OpenGL state: \n" << std::endl;
-    std::cout << GlState::queryAll() << std::endl;
-  }
-  ::testing::InitGoogleTest(&argc, argv);
+        std::cout << "Device's current OpenGL state: \n" << std::endl;
+        std::cout << GlState::queryAll() << std::endl;
+    }
+    ::testing::InitGoogleTest(&argc, argv);
 
-  return RUN_ALL_TESTS();
+    return RUN_ALL_TESTS();
 }
